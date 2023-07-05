19f2a53d18c1fbdad59a5a5a680c4a61fe6e8548
Mizuki <136825912+MizukiEmmei@users.noreply.github.com>
2023-07-05 10:19:18 +0900

    Update stable_sort.cpp

--- Library/stable_sort.cpp
+++ Library/stable_sort.cpp
@@ -1,40 +1,6 @@
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
-#include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <streambuf>
-#include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
-#include <vector>
-#include <array>
-using namespace std;
-#define rep(i, n) for(int i = 0; i < n; i++)
-
+//安定ソート　：　同一の値が存在した時,元の順序を保持することを保証する。
 //ABC308 - C
+
 int main(){
     vector< pair<int,int> > c(10);
     rep(i,10)c[i].first = i;
